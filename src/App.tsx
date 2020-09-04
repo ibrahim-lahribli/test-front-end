65d5dbe8de703d4cb8cc75f4a033ba765f40dc96
ilahribli <ibrahim@hireme.io>
2020-09-04 17:32:35 +0100

    add react router && configure app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NotFoundPage } from "./pages/NotFoundPage";
+import { Form } from "./pages/Form";
+import { List } from "./pages/List";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/form">Form</Link>
+            <Link to="/list">List</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/form" component={Form} />
+            <Route exact path="/list" component={List} />
+            <Route path="*" component={NotFoundPage} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
